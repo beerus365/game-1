cdb620e8a0acedd5ffcf024dacb5f623e4beaeae
beerus <ajdbriones01202401029@usep.edu.ph>
2026-01-16 22:20:57 +0800

    added enemy animations

--- scripts/goblin.gd
+++ scripts/goblin.gd
@@ -1,17 +1,34 @@
 extends CharacterBody2D
 
 @export var SPEED = 50
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 var player_chase = false
 var player = null
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta: float) -> void:	
 	if player_chase and player:
 		var direction = (player.global_position - global_position).normalized()
 		velocity = direction * SPEED
+		
+			# Animation Handler
+		var dir_x : float = player.global_position.x - global_position.x
+		if dir_x > 0:
+			animated_sprite.flip_h = false
+		elif dir_x < 0:
+			animated_sprite.flip_h = true
+			
+		if velocity.length() > 0:
+			animated_sprite.play("run")
+		else:
+			animated_sprite.play("idle")
 	else:
 		velocity = Vector2.ZERO
+		animated_sprite.play("idle")
 		
 	move_and_slide()
+
+
+	
 
 func _on_detect_body_entered(body):
 	if body.is_in_group("Player"):
