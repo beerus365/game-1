9430d90a9c10ba4a5b7e36300c5607dd6d5c9bc2
beerus <ajdbriones01202401029@usep.edu.ph>
2026-01-18 10:36:50 +0800

    added jump obstacles

--- scripts/goblin.gd
+++ scripts/goblin.gd
@@ -1,40 +1,52 @@
 extends CharacterBody2D
 
-@export var SPEED = 50
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-var player_chase = false
-var player = null
+const SPEED = 50
+const JUMP_VELOCITY = -275
 
-func _physics_process(delta: float) -> void:	
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var ray_right: RayCast2D = $raycast_right
+@onready var ray_left: RayCast2D = $raycast_left
+
+var player: Node2D = null
+var player_chase := false
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += get_gravity().y * delta
+
 	if player_chase and player:
-		var direction = (player.global_position - global_position).normalized()
-		velocity = direction * SPEED
+		var dir = sign(player.global_position.x - global_position.x)
+		velocity.x = dir * SPEED
 		
-		# Animation Handler
-		var dir_x : float = player.global_position.x - global_position.x
-		if dir_x > 0:
-			animated_sprite.flip_h = false
-		elif dir_x < 0:
-			animated_sprite.flip_h = true
-			
-		if velocity.length() > 0:
-			animated_sprite.play("run")
-		else:
-			animated_sprite.play("idle")
+		# Flip sprite
+		animated_sprite.flip_h = dir < 0
+		
+		# Jump abstacles
+		if is_on_floor() and obstacle_in_direction(dir):
+			velocity.y = JUMP_VELOCITY
+		
+		animated_sprite.play("run")
 	else:
-		velocity = Vector2.ZERO
+		velocity.x = 0
 		animated_sprite.play("idle")
-		
+
 	move_and_slide()
 
+
+func obstacle_in_direction(dir: int) -> bool:
+	if dir > 0:
+		return ray_right.is_colliding()
+	elif dir < 0:
+		return ray_left.is_colliding()
+	return false
+
 func _on_detect_body_entered(body: Node2D) -> void:
-	if body is Player:
-		print("hello", body.name)
+	if body.is_in_group("Player"):
 		player = body
 		player_chase = true
 
 func _on_detect_body_exited(body: Node2D) -> void:
-	if body is Player:
+	if body.is_in_group("Player"):
 		player = null
 		player_chase = false
-	
